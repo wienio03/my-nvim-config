71bc889a632ee1ab46be0ea94b3926d3416cb4d1
Wienio <115157086+wienio03@users.noreply.github.com>
2023-07-24 11:53:26 +0200

    Update lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -22,7 +22,7 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
-
+require('luasnip.loaders.from_vscode').lazy_load()
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
 })
