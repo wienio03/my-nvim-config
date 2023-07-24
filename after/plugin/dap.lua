d1e895dbe3dff4e03e731cdec1db4bff37486558
vvienio <wienczyslaw.wlodyga@student.uj.edu.pl>
2023-07-24 13:21:29 +0200

    nvim DAP config added

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,4 +1,6 @@
 --bindings
-vim.api.nvim_set_keymap('n', '<leader>dt', ':DapUIToggle<CR>', {noremap=true})
+require("dapui").setup()
+vim.api.nvim_set_keymap('n', '<leader>dt', ':lua require("dapui").open()<CR>', {noremap=true})
 vim.api.nvim_set_keymap('n', '<leader>db', 'DapToggleBreakpoint<CR>', {noremap=true})
-vim.api.nvim_set_keymap('n', '<leader>dc', ':DapContinue<CR>', {noremap=true})
+vim.api.nvim_set_keymap('n', '<leader>dc', ':lua require("dapui").close()<CR>', {noremap=true})
+
