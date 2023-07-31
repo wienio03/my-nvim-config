e19c7f398190411390209347d1e74e1a832e5ffe
vvienio <wienczyslaw.wlodyga@student.uj.edu.pl>
2023-07-31 11:57:09 +0200

    colorcolumn removed

--- after/plugin/dressing.lua
+++ after/plugin/dressing.lua
@@ -16,7 +16,6 @@ require('dressing').setup({
     start_in_insert = true,
 
     -- These are passed to nvim_open_win
-    anchor = "SW",
     border = "rounded",
     -- 'editor' and 'win' will default to being centered
     relative = "cursor",
