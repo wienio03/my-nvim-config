31109bb536137050ec0ba35dafea600e53df0e84
wienio03 <wienczyslaw.wlodyga@student.uj.edu.pl>
2023-08-31 22:50:00 +0200

    changes

--- lua/wienio/plugins/leetcode.lua
+++ lua/wienio/plugins/leetcode.lua
@@ -1,5 +1,10 @@
 local g = vim.g
 
-g.leetcode_browser = "chrome"
+g.python3_host_prog = "C:/Users/nenek/AppData/Local/Programs/Python/Python311/python.exe"
+--remember to run <PYTHON_ABSOLUTE_PATH> -m pip install keyring pynvim browser-cookie3-
+
+g.leetcode_browser = "edge"
 
 g.leetcode_solution_filetype = "cpp"
+
+g.leetcode_china = 0
