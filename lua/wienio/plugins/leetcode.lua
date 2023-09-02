877dcd1fef99a2fd355b47c24819df879f23109b
Wienio <wienczyslaw.wlodyga@student.uj.edu.pl>
2023-09-03 01:34:11 +0200

    changes

--- lua/wienio/plugins/leetcode.lua
+++ lua/wienio/plugins/leetcode.lua
@@ -1,9 +1,9 @@
 local g = vim.g
 
-g.python3_host_prog = "C:/Users/nenek/AppData/Local/Programs/Python/Python311/python.exe"
+g.python3_host_prog = "C:/Users/joann/AppData/Local/Programs/Python/Python39/python.exe"
 --remember to run <PYTHON_ABSOLUTE_PATH> -m pip install keyring pynvim browser-cookie3-
-
-g.leetcode_browser = "edge"
+--Python 3.9.13 is advised (used in my case)
+g.leetcode_browser = "chrome"
 
 g.leetcode_solution_filetype = "cpp"
 
